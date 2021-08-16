2401a36ca06eaed39a8b0a73523b39dc8c1e9d9d
Ezequiel Villarreal <evillarreal@smd.com.pa>
2021-08-15 21:51:50 -0500

    Update AWSMail.php

--- classes/Kohana/AWSMail.php
+++ classes/Kohana/AWSMail.php
@@ -1,23 +1,23 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php
 
 class Kohana_AWSMail {
 
-	public static function message()
+	public static function message(): AWSMail_Services_Message
 	{
 		return new AWSMail_Services_Message();
 	}
 
-	public static function template()
+	public static function template(): AWSMail_Services_Template
 	{
 		return new AWSMail_Services_Template();
 	}
 
-	public static function destinations()
+	public static function destinations(): Kohana_Services_Destinations
 	{
 		return new AWSMail_Services_Destinations();
 	}
 
-	public static function utility()
+	public static function utility(): AWSMail_Services_Utility
 	{
 		return new AWSMail_Services_Utility();
 	}
